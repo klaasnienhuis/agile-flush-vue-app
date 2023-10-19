fa38a028bc1bd6441f68c0bd5f6ff80540b4c071
Klaas Nienhuis <mail@klaasnienhuis.nl>
2023-10-19 11:29:19 +0200

    make suitable for vercel

--- api/createTokenRequest/index.js
+++ api/createTokenRequest/index.js
@@ -1,11 +1,8 @@
-const Ably = require('ably/promises');
+const Ably = require("ably/promises");
 
-module.exports = async function (context) {
-    const id = `id- + ${Math.random().toString(36).substr(2, 16)}`;
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
-    const tokenRequestData = await client.auth.createTokenRequest({ clientId: id });
-    context.res = { 
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify(tokenRequestData) 
-    };
-};
+export default async function handler(request, response) {
+  const id = `id- + ${Math.random().toString(36).substr(2, 16)}`;
+  const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+  const tokenRequestData = await client.auth.createTokenRequest({ clientId: id });
+  return response.status(200).json(tokenRequestData);
+}
